502643903dec9067eebce74c66d23b5b9e5cfa72
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-09 22:15:03 +0600

    new code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>@gmail.com</span>
+        <span>ma@gmail.com</span>
         <div className="f-icons">
           <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
